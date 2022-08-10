6e834a977c3d38054df4f2d17a90d41cf6b15383
joilsonlira <joilson.lira@live.com>
2022-08-10 19:31:58 -0300

    UPDATE: inicio da interação do slide da pagina de negocios e atletas

--- scripts/js/global.js
+++ scripts/js/global.js
@@ -1,11 +1,14 @@
-var _a;
+var _a, _b;
 var main_header = document.getElementById("main_header");
 var row_slide_img = document.querySelectorAll(".slide_item");
+var negocio_slide_img = document.querySelectorAll(".empresa_logo");
 var _loop_1 = function (i) {
     var destaque_img = (_a = row_slide_img[i].firstElementChild) === null || _a === void 0 ? void 0 : _a.getAttribute("src");
+    var destaque_img_negocio = (_b = negocio_slide_img[i].firstElementChild) === null || _b === void 0 ? void 0 : _b.getAttribute("src");
     setInterval(function () {
-        var _a;
+        var _a, _b;
         console.log((_a = row_slide_img[i].firstElementChild) === null || _a === void 0 ? void 0 : _a.setAttribute("src", destaque_img));
+        console.log((_b = row_slide_img[i].firstElementChild) === null || _b === void 0 ? void 0 : _b.setAttribute("src", destaque_img_negocio));
         row_slide_img[1].classList.toggle('dest');
         console.log(destaque_img);
     }, 3000);
--- scripts/js/global.ts
+++ scripts/js/global.ts
@@ -1,10 +1,13 @@
 const main_header = <HTMLElement>document.getElementById("main_header");
 const row_slide_img = document.querySelectorAll(".slide_item");
+const negocio_slide_img = document.querySelectorAll(".empresa_logo");
 
 for(let i: number = 0; i < row_slide_img.length;i++){
     let destaque_img: string = row_slide_img[i].firstElementChild?.getAttribute("src");
+    let destaque_img_negocio: string = negocio_slide_img[i].firstElementChild?.getAttribute("src");
     setInterval(() => {
         console.log(row_slide_img[i].firstElementChild?.setAttribute("src", destaque_img));
+        console.log(row_slide_img[i].firstElementChild?.setAttribute("src", destaque_img_negocio));
         row_slide_img[1].classList.toggle('dest');
         console.log(destaque_img);
     }, 3000)
