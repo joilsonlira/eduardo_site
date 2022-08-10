b1d9c50c73b4a25117a7b46b4832d285f946982a
joilsonlira <joilson.lira@live.com>
2022-08-10 18:55:36 -0300

    UPDATE: inicio da interação do slide

--- scripts/js/global.js
+++ scripts/js/global.js
@@ -1,1 +1,11 @@
-var main_header = document.getElementById('main_header');
+var main_header = document.getElementById("main_header");
+var row_slide_img = document.querySelectorAll(".slide_item");
+setInterval(function () {
+    var _a, _b;
+    for (var i = 0; i < row_slide_img.length; i++) {
+        var destaque_img = (_a = row_slide_img[i].firstElementChild) === null || _a === void 0 ? void 0 : _a.getAttribute("src");
+        console.log((_b = row_slide_img[1].firstElementChild) === null || _b === void 0 ? void 0 : _b.setAttribute("src", destaque_img));
+        row_slide_img[1].classList.toggle('dest');
+        console.log(i);
+    }
+}, 3000);
--- scripts/js/global.ts
+++ scripts/js/global.ts
@@ -1,2 +1,17 @@
-const main_header = <HTMLElement>document.getElementById('main_header');
+const main_header = <HTMLElement>document.getElementById("main_header");
+const row_slide_img = document.querySelectorAll(".slide_item");
+
+setInterval(() => {
+    for(let i: number = 0; i < row_slide_img.length;i++){
+        let destaque_img: string = row_slide_img[i].firstElementChild?.getAttribute("src");
+        console.log(row_slide_img[1].firstElementChild?.setAttribute("src", destaque_img));
+        row_slide_img[1].classList.toggle('dest')
+        console.log(i);        
+        
+        }
+        
+    }, 3000)
+
+
+
 
