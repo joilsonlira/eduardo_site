9c8d4bbf11b47c07285d15243ce6f3c0a4b51dac
joilsonlira <joilson.lira@live.com>
2022-08-10 18:57:17 -0300

    UPDATE: inicio da interação do slide

--- scripts/js/global.ts
+++ scripts/js/global.ts
@@ -1,16 +1,16 @@
 const main_header = <HTMLElement>document.getElementById("main_header");
 const row_slide_img = document.querySelectorAll(".slide_item");
 
-setInterval(() => {
-    for(let i: number = 0; i < row_slide_img.length;i++){
+for(let i: number = 0; i < row_slide_img.length;i++){
+    setInterval(() => {
+    
         let destaque_img: string = row_slide_img[i].firstElementChild?.getAttribute("src");
         console.log(row_slide_img[1].firstElementChild?.setAttribute("src", destaque_img));
         row_slide_img[1].classList.toggle('dest')
         console.log(i);        
-        
-        }
-        
+            
     }, 3000)
+}
 
 
 
