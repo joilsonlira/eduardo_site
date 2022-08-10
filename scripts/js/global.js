52c630dd7ae581785ad457245d1ea9b78a71c8c9
joilsonlira <joilson.lira@live.com>
2022-08-10 19:23:28 -0300

    UPDATE: inicio da interação do slide

--- scripts/js/global.js
+++ scripts/js/global.js
@@ -1,11 +1,15 @@
+var _a;
 var main_header = document.getElementById("main_header");
 var row_slide_img = document.querySelectorAll(".slide_item");
-setInterval(function () {
-    var _a, _b;
-    for (var i = 0; i < row_slide_img.length; i++) {
-        var destaque_img = (_a = row_slide_img[i].firstElementChild) === null || _a === void 0 ? void 0 : _a.getAttribute("src");
-        console.log((_b = row_slide_img[1].firstElementChild) === null || _b === void 0 ? void 0 : _b.setAttribute("src", destaque_img));
+var _loop_1 = function (i) {
+    var destaque_img = (_a = row_slide_img[i].firstElementChild) === null || _a === void 0 ? void 0 : _a.getAttribute("src");
+    setInterval(function () {
+        var _a;
+        console.log((_a = row_slide_img[i].firstElementChild) === null || _a === void 0 ? void 0 : _a.setAttribute("src", destaque_img));
         row_slide_img[1].classList.toggle('dest');
-        console.log(i);
-    }
-}, 3000);
+        console.log(destaque_img);
+    }, 3000);
+};
+for (var i = 0; i < row_slide_img.length; i++) {
+    _loop_1(i);
+}
--- scripts/js/global.ts
+++ scripts/js/global.ts
@@ -2,13 +2,11 @@ const main_header = <HTMLElement>document.getElementById("main_header");
 const row_slide_img = document.querySelectorAll(".slide_item");
 
 for(let i: number = 0; i < row_slide_img.length;i++){
+    let destaque_img: string = row_slide_img[i].firstElementChild?.getAttribute("src");
     setInterval(() => {
-    
-        let destaque_img: string = row_slide_img[i].firstElementChild?.getAttribute("src");
-        console.log(row_slide_img[1].firstElementChild?.setAttribute("src", destaque_img));
-        row_slide_img[1].classList.toggle('dest')
-        console.log(i);        
-            
+        console.log(row_slide_img[i].firstElementChild?.setAttribute("src", destaque_img));
+        row_slide_img[1].classList.toggle('dest');
+        console.log(destaque_img);
     }, 3000)
 }
 
